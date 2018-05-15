5b545e5068fde29f44d6dea7188f09e9393369a4
Timur Usmanov <heytimy@gmail.com>
2018-05-15 21:27:32 +0500

    Done long time ago, just making sure everything is uptodate

---
+++ webpage/music.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>Music Viewer</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link href="viewer.css" type="text/css" rel="stylesheet" />
+	</head>
+	<body>
+		<div id="header">
+
+			<h1>190M Music Playlist Viewer</h1>
+			<h2>Search Through Your Playlists and Music</h2>
+		</div>
+
+		<?php 
+			$folder = 'songs/';
+			$songs = scandir($folder);
+		 ?>
+		<div id="listarea">
+			<ul id="musiclist">
+
+
+				<?php 
+					foreach ($songs as $song){
+						if (preg_match("/(\.mp3)$/", $song)){
+				
+				 ?>
+
+				<li class="mp3item">
+					<?php print($folder . $url_name) ?>
+					<a href=<?= $folder . urlencode($song) ?>>
+						<?php print($song); } ?>
+					</a>
+				</li>
+				<?php } ?>
+			</ul>
+		</div>
+	</body>
+</html>
